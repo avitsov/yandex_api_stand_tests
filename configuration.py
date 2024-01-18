9b75937967f927531b8c667a9748cca7dd9ff32c
Cristina Baranova <armasarkristinf7289@yande.ru>
2024-01-18 12:04:43 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = ("https://4c833cf3-26af-4a31-b1c0-6d95b5fcb24b.serverhub.praktikum-services.ru")
+DOC_PATH = "/docs/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
